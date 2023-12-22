82f8669a0f36faf5c06069189fb9acac348cbe99
Abdullah26648 <abdullah26648@gmail.com>
2023-12-22 20:31:44 +0300

    21.12.2023 5. day progress. Whole project colorcoded and some methods modified. This is last commit :).

--- src/Warehouse_Management.java
+++ src/Warehouse_Management.java
@@ -11,82 +11,82 @@ public class Warehouse_Management {
     public static final String B = "\u001B[34m"; // Blue
 
     public void createProduct() {
-        System.out.println(Y +"✒️ Enter product details:");
-        System.out.print(W +"💾 ID: ");
+        System.out.println(Y + "✒️ Enter product details:");
+        System.out.print(W + "💾 ID: ");
         int id = Input_Exception_Handler.getIntInput("");
         if (isProductIdExists(id)) {
-            System.out.println(R +"‼️ Product with ID " + id + " already exists. Please enter a different ID.");
+            System.out.println(R + "‼️ Product with ID " + id + " already exists. Please enter a different ID.");
             return;
         }
-        System.out.print(W +"📕 Name: ");
+        System.out.print(W + "📕 Name: ");
         String name = Input_Exception_Handler.getNonBlankStringInput("");
-        System.out.print(W +"📗 Producer: ");
+        System.out.print(W + "📗 Producer: ");
         String producer = Input_Exception_Handler.getNonBlankStringInput("");
-        System.out.print(W +"🗃️ Quantity: ");
+        System.out.print(W + "🗃️ Quantity: ");
         int quantity = Input_Exception_Handler.getIntInput("");
-        System.out.print(W +"📢 Unit: ");
+        System.out.print(W + "📢 Unit: ");
         String unit = Input_Exception_Handler.getNonBlankStringInput("");
-        System.out.print(W +"📃 Shelf: ");
+        System.out.print(W + "📃 Shelf: ");
         String shelf = Input_Exception_Handler.getNonBlankStringInput("");
 
         Warehouse_Product warehouseProduct = new Warehouse_Product(id, name, producer, quantity, unit, shelf);
         productsMap.computeIfAbsent(id, k -> new ArrayList<>()).add(warehouseProduct);
-        System.out.println(G +"✔️ Product created successfully.");
+        System.out.println(G + "✔️ Product created successfully.");
     }
 
     public void enterProduct() {
-        System.out.print(Y +"✒️ Enter product ID: ");
+        System.out.print(Y + "✒️ Enter product ID: ");
         int id = Input_Exception_Handler.getIntInput("");
 
         List<Warehouse_Product> warehouseProductList = productsMap.get(id);
 
         if (isProductIdExists(id) && warehouseProductList != null && !warehouseProductList.isEmpty()) {
 
-            System.out.print(Y +"✒️ Enter quantity to enter: ");
+            System.out.print(Y + "✒️ Enter quantity to enter: ");
             int quantityToAdd = Input_Exception_Handler.getIntInput("");
 
             for (Warehouse_Product warehouseProduct : warehouseProductList) {
                 int currentQuantity = warehouseProduct.getQuantity();
                 warehouseProduct.setQuantity(currentQuantity + quantityToAdd);
             }
-            System.out.println(G +"✔️ Product quantity updated successfully.");
+            System.out.println(G + "✔️ Product quantity updated successfully.");
 
             for (Warehouse_Product warehouseProduct : warehouseProductList) {
-                System.out.println(G +"🗃️ Updated Product: " + warehouseProduct);
+                System.out.println(G + "🗃️ Updated Product: " + warehouseProduct);
             }
         } else {
             int answer;
             do {
-                System.out.println(R +"‼️ Product with ID " + id + " not found.");
-                System.out.println(R +"❓ Would you like to create product ❓");
-                System.out.println(W +"1️⃣ for Yes");
-                System.out.println(W +"2️⃣ for No");
-                System.out.print(Y +"✒️ Enter your choice: ");
+                System.out.println(R + "‼️ Product with ID " + id + " not found.");
+                System.out.println(R + "❓ Would you like to create product ❓");
+                System.out.println(W + "1️⃣ for Yes");
+                System.out.println(W + "2️⃣ for No");
+                System.out.print(Y + "✒️ Enter your choice: ");
 
                 answer = Input_Exception_Handler.getIntInput("");
 
                 if (answer == 1) {
                     createProduct();
                 } else if (answer == 2) {
-                    System.out.println(B +"🚪 Returning to the Manager Menu...");
+                    System.out.println(B + "🚪 Returning to the Manager Menu...");
                     Loading();
                 } else {
-                    System.out.println(R +"‼️ Please enter 1 or 2. Try again.");
+                    System.out.println(R + "‼️ Please enter 1 or 2. Try again.");
                 }
             } while (answer != 1 && answer != 2);
         }
     }
 
     public void exitProduct() {
-        System.out.print(Y +"✒️ Enter product ID to exit: ");
+        System.out.print(Y + "✒️ Enter product ID to exit: ");
         int id = Input_Exception_Handler.getIntInput("");
 
         if (!isProductIdExists(id)) {
-            System.out.println(R +"‼️ Product with ID " + id + " not exists.");
+            System.out.println(R + "‼️ Product with ID " + id + " not exists.");
             return;
         }
 
-        System.out.print(Y +"✒️ Enter quantity to exit: ");
+        System.out.print(Y + "✒️ Enter quantity to exit: ");
         int exitQuantity = Input_Exception_Handler.getIntInput("");
 
         List<Warehouse_Product> warehouseProductList = productsMap.get(id);
@@ -95,61 +95,90 @@ public class Warehouse_Management {
                 int remainingQuantity = warehouseProduct.getQuantity() - exitQuantity;
                 if (remainingQuantity > 0) {
                     warehouseProduct.setQuantity(remainingQuantity);
-                    System.out.println(G +"✔️ Product exited successfully.");
-                    System.out.println(W +"🗃️ Updated Product: " + warehouseProduct);
-                } else if (remainingQuantity == 0){
+                    System.out.println(G + "✔️ Product exited successfully.");
+                    System.out.println(W + "🗃️ Updated Product: " + warehouseProduct);
+                } else if (remainingQuantity == 0) {
                     int answer;
                     do {
-                        System.out.println(R +"‼️ Product with ID " + id + " quantity reached 0.");
-                        System.out.println(R +"❓ Would you like to delete product ❓");
-                        System.out.println(W +"1️⃣ for Yes");
-                        System.out.println(W +"2️⃣ for No");
-                        System.out.print(Y +"✒️ Enter your choice: ");
+                        System.out.println(R + "‼️ Product with ID " + id + " quantity reached 0.");
+                        System.out.println(R + "❓ Would you like to delete product ❓");
+                        System.out.println(W + "1️⃣ for Yes");
+                        System.out.println(W + "2️⃣ for No");
+                        System.out.print(Y + "✒️ Enter your choice: ");
 
                         answer = Input_Exception_Handler.getIntInput("");
 
                         if (answer == 1) {
                             deleteProduct();
                         } else if (answer == 2) {
-                            System.out.println(B +"🚪 Returning to the Manager Menu...");
+                            System.out.println(B + "🚪 Returning to the Manager Menu...");
                             Loading();
                         } else {
-                            System.out.println(R +"‼️ Please enter 1 or 2. Try again.");
+                            System.out.println(R + "‼️ Please enter 1 or 2. Try again.");
                         }
                     } while (answer != 1 && answer != 2);
 
                 } else {
-                    System.out.println(R +"‼️ Exit quantity for product with ID " + id + " exceeds available quantity.");
-                    System.out.println(W +"🗃️ Available Quantity: " + warehouseProduct.getQuantity());
+                    System.out.println(R + "‼️ Exit quantity for product with ID " + id + " exceeds available quantity.");
+                    System.out.println(W + "🗃️ Available Quantity: " + warehouseProduct.getQuantity());
                 }
             }
         } else {
-            System.out.println(R +"‼️ Product with ID " + id + " not found.");
+            System.out.println(R + "‼️ Product with ID " + id + " not found.");
+        }
+    }
+
+    public void buyProduct() {
+        System.out.print(Y + "✒️ Enter product ID to buy: ");
+        int id = Input_Exception_Handler.getIntInput("");
+
+        if (!isProductIdExists(id)) {
+            System.out.println(R + "‼️ Product with ID " + id + " not exists.");
+            return;
+        }
+
+        System.out.print(Y + "✒️ Enter quantity to buy: ");
+        int exitQuantity = Input_Exception_Handler.getIntInput("");
+
+        List<Warehouse_Product> warehouseProductList = productsMap.get(id);
+        if (warehouseProductList != null && !warehouseProductList.isEmpty()) {
+            for (Warehouse_Product warehouseProduct : warehouseProductList) {
+                int remainingQuantity = warehouseProduct.getQuantity() - exitQuantity;
+                if (remainingQuantity >= 0) {
+                    warehouseProduct.setQuantity(remainingQuantity);
+                    System.out.println(G + "🛒✔️ Product purchase successful.");
+                } else {
+                    System.out.println(R + "‼️ Exit quantity for product with ID " + id + " exceeds available quantity.");
+                    System.out.println(W + "🗃️ Available Quantity: " + warehouseProduct.getQuantity());
+                }
+            }
+        } else {
+            System.out.println(R + "‼️ Product with ID " + id + " not found.");
         }
     }
 
     public void updateProduct() {
-        System.out.print(Y +"✒️ Enter product ID to update: ");
+        System.out.print(Y + "✒️ Enter product ID to update: ");
         int id = Input_Exception_Handler.getIntInput("");
 
         List<Warehouse_Product> warehouseProductList = productsMap.get(id);
 
         if (warehouseProductList != null && !warehouseProductList.isEmpty()) {
-            System.out.println(Y +"✒️ Enter updated product details:");
+            System.out.println(Y + "✒️ Enter updated product details:");
 
-            System.out.print(W +"📕 Name: ");
+            System.out.print(W + "📕 Name: ");
             String name = Input_Exception_Handler.getNonBlankStringInput("");
 
-            System.out.print(W +"📗 Producer: ");
+            System.out.print(W + "📗 Producer: ");
             String producer = Input_Exception_Handler.getNonBlankStringInput("");
 
-            System.out.print(W +"🗃️ Quantity: ");
+            System.out.print(W + "🗃️ Quantity: ");
             int quantity = Input_Exception_Handler.getIntInput("");
 
-            System.out.print(W +"📢 Unit: ");
+            System.out.print(W + "📢 Unit: ");
             String unit = Input_Exception_Handler.getNonBlankStringInput("");
 
-            System.out.print(W +"📃 Shelf: ");
+            System.out.print(W + "📃 Shelf: ");
             String shelf = Input_Exception_Handler.getNonBlankStringInput("");
 
             Warehouse_Product updatedProduct = new Warehouse_Product(id, name, producer, quantity, unit, shelf);
@@ -159,37 +188,37 @@ public class Warehouse_Management {
 
                 if (currentProduct.getId() == id) {
                     warehouseProductList.set(i, updatedProduct);
-                    System.out.println(G +"✔️ Product updated successfully.");
+                    System.out.println(G + "✔️ Product updated successfully.");
 
-                    System.out.println(W +"🗃️ Updated Product: " + updatedProduct);
+                    System.out.println(W + "🗃️ Updated Product: " + updatedProduct);
                     return;
                 }
             }
         } else {
-            System.out.println(R +"‼️ Product with ID " + id + " not found.");
-            System.out.println(B +"🚪 Returning to the Manager Menu...");
+            System.out.println(R + "‼️ Product with ID " + id + " not found.");
+            System.out.println(B + "🚪 Returning to the Manager Menu...");
             Loading();
         }
     }
 
     public void deleteProduct() {
-        System.out.print(Y +"✒️ Enter product ID to delete: ");
+        System.out.print(Y + "✒️ Enter product ID to delete: ");
         int id = Input_Exception_Handler.getIntInput("");
 
         List<Warehouse_Product> warehouseProductList = productsMap.get(id);
 
         if (warehouseProductList != null && !warehouseProductList.isEmpty()) {
             productsMap.remove(id);
-            System.out.println(G +"✔️ Product deleted successfully.");
+            System.out.println(G + "✔️ Product deleted successfully.");
         } else {
-            System.out.println(R +"‼️ Product with ID " + id + " not found.");
-            System.out.println(B +"🚪 Returning to the Manager Menu...");
+            System.out.println(R + "‼️ Product with ID " + id + " not found.");
+            System.out.println(B + "🚪 Returning to the Manager Menu...");
             Loading();
         }
     }
 
     public void listProducts() {
-        System.out.println(W +"🗃️ List of Products:");
+        System.out.println(W + "🗃️ List of Products:");
 
         productsMap.values().stream()
                 .flatMap(List::stream)
@@ -215,6 +244,7 @@ public class Warehouse_Management {
         initializeProduct(1009, "Üzüm", "Manisa", 105, "Kasa", "Gıda");
         initializeProduct(1010, "Muz", "Anamur", 105, "Kasa", "Gıda");
     }
+
     public void Loading() {
         for (int i = 0; i < 30; i++) {
             System.out.print("💲");
@@ -233,14 +263,14 @@ public class Warehouse_Management {
 
     public void authorizationMenu() {
         System.out.println("\n⚜️⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⚜️");
-        System.out.println(Y +"🔐 Welcome to the Authorization Menu 🔐");
-        System.out.println(W +"🔐 Please choose your role 🕵️");
-        System.out.println(G +"1️⃣ 🛍️ Customer");
-        System.out.println(B +"2️⃣ 🛠️ Manager");
-        System.out.println(R +"0️⃣ 🚪 Exit");
+        System.out.println(Y + "🔐 Welcome to the Authorization Menu 🔐");
+        System.out.println(W + "🔐 Please choose your role 🕵️");
+        System.out.println(G + "1️⃣ 🛍️ Customer");
+        System.out.println(B + "2️⃣ 🛠️ Manager");
+        System.out.println(R + "0️⃣ 🚪 Exit");
         System.out.println("⚜️⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⚜️");
 
-        System.out.print(Y +"✒️ Enter your choice: ");
+        System.out.print(Y + "✒️ Enter your choice: ");
         int roleChoice = Input_Exception_Handler.getIntInput("");
 
         switch (roleChoice) {
@@ -251,35 +281,35 @@ public class Warehouse_Management {
                 ManagerMenu();
                 break;
             case 0:
-                System.out.println(G +"👋 Exiting the system. Goodbye! 👋");
+                System.out.println(G + "👋 Exiting the system. Goodbye! 👋");
                 System.exit(0);
             default:
-                System.out.println(R +"‼️ Invalid choice. Please enter a valid option.");
+                System.out.println(R + "‼️ Invalid choice. Please enter a valid option.");
                 Loading();
         }
     }
 
     public void ManagerMenu() {
-        System.out.print(Y +"✒️ Enter manager password: ");
+        System.out.print(Y + "✒️ Enter manager password: ");
         String enteredPassword = scanner.nextLine();
 
         if (enteredPassword.equals(MANAGER_PASSWORD)) {
-            System.out.println(G +"✔️ Successfully entered manager mode.");
+            System.out.println(G + "✔️ Successfully entered manager mode.");
             Loading();
             int choice;
             do {
-                System.out.println(B +"\n🛠️ Manager Menu 🛠️");
+                System.out.println(B + "\n🛠️ Manager Menu 🛠️");
                 System.out.println("⚜️⭐⭐⭐⭐⭐⭐⭐⭐⚜️");
-                System.out.println(G +"1️⃣ 🗃️ Create Product");
-                System.out.println(G +"2️⃣ 📥 Enter Product");
-                System.out.println(R +"3️⃣ 📤 Exit Product");
-                System.out.println(B +"4️⃣ 🔄 Update Product");
-                System.out.println(R +"5️⃣ 🗑️ Delete Product");
-                System.out.println(B +"6️⃣ 📋 List Products");
-                System.out.println(R +"0️⃣ 🚪 Exit");
+                System.out.println(G + "1️⃣ 🗃️ Create Product");
+                System.out.println(G + "2️⃣ 📥 Enter Product");
+                System.out.println(R + "3️⃣ 📤 Exit Product");
+                System.out.println(B + "4️⃣ 🔄 Update Product");
+                System.out.println(R + "5️⃣ 🗑️ Delete Product");
+                System.out.println(B + "6️⃣ 📋 List Products");
+                System.out.println(R + "0️⃣ 🚪 Exit");
                 System.out.println("⚜️⭐⭐⭐⭐⭐⭐⭐⭐⚜️");
 
-                System.out.print(Y +"✒️ Enter your choice: ");
+                System.out.print(Y + "✒️ Enter your choice: ");
                 choice = Input_Exception_Handler.getIntInput("");
                 switch (choice) {
                     case 1:
@@ -301,17 +331,17 @@ public class Warehouse_Management {
                         listProducts();
                         break;
                     case 0:
-                        System.out.println(B +"🚪 Returning to the Authorization Menu...");
+                        System.out.println(B + "🚪 Returning to the Authorization Menu...");
                         Loading();
                         authorizationMenu();
                         break;
                     default:
-                        System.out.println(R +"‼️ Invalid choice. Please enter a valid option.");
+                        System.out.println(R + "‼️ Invalid choice. Please enter a valid option.");
                 }
 
             } while (choice != 0);
         } else {
-            System.out.println(R +"‼️ Incorrect password. Access denied.");
+            System.out.println(R + "‼️ Incorrect password. Access denied.");
             Loading();
             authorizationMenu();
         }
@@ -320,29 +350,29 @@ public class Warehouse_Management {
     public void CustomerMenu() {
         int choice;
         do {
-            System.out.println(G +"\n🛍️ Customer Menu 🛍️");
+            System.out.println(G + "\n🛍️ Customer Menu 🛍️");
             System.out.println("⚜️⭐⭐⭐⭐⭐⭐⭐⭐⚜️");
-            System.out.println(G +"1️⃣ 🛒 Buy Product");
-            System.out.println(B +"2️⃣ 📋 List Products");
-            System.out.println(R +"0️⃣ 🚪 Exit");
+            System.out.println(G + "1️⃣ 🛒 Buy Product");
+            System.out.println(B + "2️⃣ 📋 List Products");
+            System.out.println(R + "0️⃣ 🚪 Exit");
             System.out.println("⚜️⭐⭐⭐⭐⭐⭐⭐⭐⚜️");
 
-            System.out.print(Y +"✒️ Enter your choice: ");
+            System.out.print(Y + "✒️ Enter your choice: ");
             choice = Input_Exception_Handler.getIntInput("");
             switch (choice) {
                 case 1:
-                    exitProduct();
+                    buyProduct();
                     break;
                 case 2:
                     listProducts();
                     break;
                 case 0:
-                    System.out.println(B +"🚪 Returning to the Authorization Menu...");
+                    System.out.println(B + "🚪 Returning to the Authorization Menu...");
                     Loading();
                     authorizationMenu();
                     break;
                 default:
-                    System.out.println(R +"‼️ Invalid choice. Please enter a valid option.");
+                    System.out.println(R + "‼️ Invalid choice. Please enter a valid option.");
             }
 
         } while (choice != 0);
